d56ffa061a3df95f01c41bdb91542e579e3709db
Sean Middleditch <sean.middleditch@gmail.com>
2021-09-13 21:37:40 -0700

    Fix clang-tidy warnings

--- include/nanofmt/std_string.h
+++ include/nanofmt/std_string.h
@@ -2,19 +2,21 @@
 
 #pragma once
 
+#include "format.h"
+
 #include <string>
 #include <string_view>
 
 namespace nanofmt {
     template <typename StringCharT, typename TraitsT, typename AllocatorT>
-    struct formatter<std::basic_string<StringCharT, TraitsT, AllocatorT>> : formatter<string_view> {
+    struct formatter<std::basic_string<StringCharT, TraitsT, AllocatorT>> : formatter<nanofmt::string_view> {
         constexpr void format(std::basic_string<StringCharT, TraitsT, AllocatorT> const& value, buffer& buf) {
             formatter<string_view>::format({value.data(), value.size()}, buf);
         }
     };
 
     template <typename StringCharT, typename TraitsT>
-    struct formatter<std::basic_string_view<StringCharT, TraitsT>> : formatter<string_view> {
+    struct formatter<std::basic_string_view<StringCharT, TraitsT>> : formatter<nanofmt::string_view> {
         constexpr void format(std::basic_string_view<StringCharT, TraitsT> const& value, buffer& buf) {
             formatter<string_view>::format({value.data(), value.size()}, buf);
         }
