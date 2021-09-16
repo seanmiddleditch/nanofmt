8a62b6da752852348add7baf6a37eb15eee7a10c
Sean Middleditch <sean.middleditch@gmail.com>
2021-09-16 09:55:46 -0700

    Add missing cstring include to tests

--- tests/test_format_value.cpp
+++ tests/test_format_value.cpp
@@ -3,6 +3,7 @@
 #include "nanofmt/format.h"
 
 #include <catch2/catch_test_macros.hpp>
+#include <cstring>
 
 TEST_CASE("nanofmt.format_value", "[nanofmt][format_value]") {
     using namespace nanofmt;
