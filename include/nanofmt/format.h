43f5811541cdea7c427fc836ec7affed760f5caf
Sean Middleditch <sean.middleditch@gmail.com>
2021-09-18 15:35:45 -0700

    Merge format_arg.h into format.h

--- include/nanofmt/format.h
+++ include/nanofmt/format.h
@@ -6,6 +6,7 @@
 #include "config.h"
 
 #include <cstddef>
+#include <type_traits>
 
 namespace NANOFMT_NS {
     struct format_args;
@@ -88,6 +89,18 @@ namespace NANOFMT_NS {
 
     namespace detail {
         char* vformat(buffer& buf, format_string format_str, format_args&& args);
+
+        // avoid explicitly pulling in <utility>
+        template <typename T>
+        const T& declval() noexcept;
+
+        template <typename ValueT>
+        constexpr format_value make_format_value(ValueT const& value) noexcept;
+
+        template <typename T, typename = void>
+        struct has_formatter;
+        template <typename T>
+        struct value_type_map;
 
         struct format_spec {
             int width = -1;
@@ -255,11 +268,130 @@ namespace NANOFMT_NS {
         format_value_to(buf, value, spec);
         return buf.advance;
     }
-} // namespace NANOFMT_NS
 
-#include "format_arg.h"
+    /// Type-erased wrapper for a formattable value.
+    struct format_value {
+        enum class type {
+            t_mono,
+            t_int,
+            t_uint,
+            t_long,
+            t_ulong,
+            t_longlong,
+            t_ulonglong,
+            t_char,
+            t_float,
+            t_double,
+            t_bool,
+            t_cstring,
+            t_voidptr,
+            t_custom
+        };
 
-namespace NANOFMT_NS {
+        struct custom {
+            void (*thunk)(void const* value, char const** spec, char const* end, buffer& buf) = nullptr;
+            void const* value = nullptr;
+        };
+
+        constexpr format_value() noexcept : v_int(0) {}
+        constexpr format_value(int value) noexcept : v_int(value), tag(type::t_int) {}
+        constexpr format_value(unsigned value) noexcept : v_unsigned(value), tag(type::t_uint) {}
+        constexpr format_value(long value) noexcept : v_long(value), tag(type::t_long) {}
+        constexpr format_value(unsigned long value) noexcept : v_ulong(value), tag(type::t_ulong) {}
+        constexpr format_value(long long value) noexcept : v_longlong(value), tag(type::t_longlong) {}
+        constexpr format_value(unsigned long long value) noexcept : v_ulonglong(value), tag(type::t_ulonglong) {}
+        constexpr format_value(char value) noexcept : v_char(value), tag(type::t_char) {}
+        constexpr format_value(float value) noexcept : v_float(value), tag(type::t_float) {}
+        constexpr format_value(double value) noexcept : v_double(value), tag(type::t_double) {}
+        constexpr format_value(bool value) noexcept : v_bool(value), tag(type::t_bool) {}
+        constexpr format_value(char const* value) noexcept : v_cstring(value), tag(type::t_cstring) {}
+        constexpr format_value(void const* value) noexcept : v_voidptr(value), tag(type::t_voidptr) {}
+        constexpr format_value(custom value) noexcept : v_custom(value), tag(type::t_custom) {}
+
+        union {
+            int v_int;
+            unsigned v_unsigned;
+            long v_long;
+            unsigned long v_ulong;
+            long long v_longlong;
+            unsigned long long v_ulonglong;
+            char v_char;
+            float v_float;
+            double v_double;
+            bool v_bool;
+            char const* v_cstring;
+            void const* v_voidptr;
+            custom v_custom;
+        };
+
+        type tag = type::t_mono;
+    };
+
+    namespace detail {
+        template <typename T, typename>
+        struct has_formatter {
+            static constexpr bool value = false;
+        };
+        template <typename T>
+        struct has_formatter<
+            T,
+            std::void_t<
+                decltype(formatter<T>{}.parse("", "")),
+                decltype(formatter<T>{}.format(declval<T>(), buffer{}))>> {
+            static constexpr bool value = true;
+        };
+
+        template <typename T>
+        struct value_type_map {
+            using type = T;
+        };
+        template <>
+        struct value_type_map<signed char> {
+            using type = signed int;
+        };
+        template <>
+        struct value_type_map<unsigned char> {
+            using type = signed int;
+        };
+        template <>
+        struct value_type_map<signed short> {
+            using type = signed int;
+        };
+        template <>
+        struct value_type_map<unsigned short> {
+            using type = signed int;
+        };
+        template <>
+        struct value_type_map<decltype(nullptr)> {
+            using type = void const*;
+        };
+
+        template <typename ValueT>
+        constexpr format_value make_format_value(ValueT const& value) noexcept {
+            using MappedT = typename detail::value_type_map<std::decay_t<ValueT>>::type;
+            if constexpr (std::is_constructible_v<format_value, MappedT>) {
+                return (MappedT)(value);
+            }
+            else if constexpr (detail::has_formatter<ValueT>::value) {
+                format_value::custom custom;
+                custom.thunk = +[](void const* value, char const** in, char const* end, buffer& buf) {
+                    formatter<ValueT> fmt;
+                    if (in != nullptr) {
+                        *in = fmt.parse(*in, end);
+                    }
+                    fmt.format(*static_cast<ValueT const*>(value), buf);
+                };
+                // this is basically std::addressof, but we want to avoid pulling in <memory> as a dependency
+                custom.value =
+                    reinterpret_cast<ValueT*>(&const_cast<char&>(reinterpret_cast<const volatile char&>(value)));
+                return custom;
+            }
+            else if constexpr (std::is_enum_v<ValueT>) {
+                return static_cast<typename detail::value_type_map<std::underlying_type_t<ValueT>>::type>(value);
+            }
+        };
+    } // namespace detail
+
     /// Holds a list of N format_value objects.
     ///
     /// This is primarily meant to be an intermediate that holds onto values
--- source/vformat.cpp
+++ source/vformat.cpp
@@ -3,8 +3,8 @@
 #include "parse_utils.h"
 #include "nanofmt/format.h"
 
-namespace NANOFMT_NS::detail {
-    char* vformat(buffer& buf, format_string format_str, format_args&& args) {
+namespace NANOFMT_NS {
+    char* detail::vformat(buffer& buf, format_string format_str, format_args&& args) {
         int arg_next_index = 0;
         bool arg_auto_index = true;
 
@@ -73,4 +73,53 @@ namespace NANOFMT_NS::detail {
         buf.append(input_begin, input - input_begin);
         return buf.pos;
     }
-} // namespace NANOFMT_NS::detail
+
+    void format_args::format(unsigned index, char const** in, char const* end, buffer& buf) const {
+        using types = format_value::type;
+
+        if (index >= count) {
+            return;
+        }
+
+        format_value const& value = values[index];
+
+        auto invoke = [in, end, &buf](auto value) {
+            formatter<decltype(value)> fmt;
+            if (in != nullptr) {
+                *in = fmt.parse(*in, end);
+            }
+            fmt.format(value, buf);
+        };
+
+        switch (value.tag) {
+            case types::t_mono:
+                return;
+            case types::t_char:
+                return invoke(value.v_char);
+            case types::t_int:
+                return invoke(value.v_int);
+            case types::t_uint:
+                return invoke(value.v_unsigned);
+            case types::t_long:
+                return invoke(value.v_long);
+            case types::t_ulong:
+                return invoke(value.v_ulong);
+            case types::t_longlong:
+                return invoke(value.v_longlong);
+            case types::t_ulonglong:
+                return invoke(value.v_ulonglong);
+            case types::t_float:
+                return invoke(value.v_float);
+            case types::t_double:
+                return invoke(value.v_double);
+            case types::t_bool:
+                return invoke(value.v_bool);
+            case types::t_cstring:
+                return invoke(value.v_cstring);
+            case types::t_voidptr:
+                return invoke(value.v_voidptr);
+            case types::t_custom:
+                return value.v_custom.thunk(value.v_custom.value, in, end, buf);
+        }
+    }
+} // namespace NANOFMT_NS
